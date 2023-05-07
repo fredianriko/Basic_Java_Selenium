a2c48bd76d9d7299e46a7d1f4e00d61fb07667cf
Fredi Anriko <fredianriko@gmail.com>
2023-05-07 19:42:01 +0700

    change

--- TestLogin/src/LoginPositive.java
+++ TestLogin/src/LoginPositive.java
@@ -1,32 +1,12 @@
+// package TestLogin;
 
 
-
+// import org.openqa.selenium.WebDriver;
 
 public class LoginPositive {
     public static void main(String[] args) {
-        // Set up the WebDriver using WebDriverManager
-        // System.setProperty("webdriver.chrome.driver", "C:/Users/CTAS039/Desktop/JAVA_Selenium"); //can be replaced
+
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\riko\\Desktop\\OPALS\\SeleniumJava\\TestLogin\\drivers\\chromedriver.exe");
-
-        // ChromeDriver driver = new ChromeDriver(); 
-
-        // Maximize the window and navigate to the login page
-        // driver.manage().window().maximize();
-        // driver.get("http://the-internet.herokuapp.com/login");
-
-        // // Enter the username and password and submit the form
-        // String username = "tomsmith";
-        // String password = "SuperSecretPassword!";
-        // WebElement usernameField = driver.findElement(By.xpath("//*[@id=\"username\"]"));
-        // WebElement passwordField = driver.findElement(By.xpath("//*[@id=\"password\"]"));
-        // WebElement submitButton = driver.findElement(By.xpath("//*[@id=\"login\"]/button"));
-        // usernameField.sendKeys(username);
-        // passwordField.sendKeys(password);
-        // submitButton.click();
-
-        // // Wait for the login to complete and then close the browser
-        // // driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        // // driver.quit();
 
         WebDriver driver = new ChromeDriver();
 
